5664fcedcdc310bd068458a85315ff4de4d21457
jiahao <monkey19911021@gmail.com>
2017-04-17 16:00:12 +0800

    添加本地化文件

--- MKNewMail/MKNewMail/ViewController.m
+++ MKNewMail/MKNewMail/ViewController.m
@@ -39,12 +39,7 @@
 
 -(void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
-    
-    //延时处理
-    dispatch_time_t start = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC));
-    dispatch_after(start, dispatch_get_main_queue(), ^{
-        [self showContact: nil];
-    });
+    [self showContact: nil];
 }
 
 -(void)configureDataSource {
@@ -101,27 +96,36 @@
 }
 
 -(void)configureContent {
-    //Set Custom CSS
-    NSString *customCSS = @"";
-    [self setCSS:customCSS];
-    
     self.alwaysShowToolbar = YES;
     self.receiveEditorDidChangeEvents = NO;
     
-    // HTML Content to set in the editor
-    NSString *html = @"<div class='test'></div><!-- This is an HTML comment -->"
-    "<p>This is a test of the <strong>ZSSRichTextEditor</strong> by <a title=\"Zed Said\" href=\"http://www.zedsaid.com\">Zed Said Studio</a></p>";
+    self.shouldShowKeyboard = YES;
+    self.enabledToolbarItems = @[ZSSRichTextEditorToolbarInsertImage,
+                                 ZSSRichTextEditorToolbarInsertImageFromDevice,
+                                 ZSSRichTextEditorToolbarInsertLink,
+                                 ZSSRichTextEditorToolbarBold,
+                                 ZSSRichTextEditorToolbarStrikeThrough,
+                                 ZSSRichTextEditorToolbarUnderline,
+                                 ZSSRichTextEditorToolbarParagraph,
+                                 ZSSRichTextEditorToolbarH1,
+                                 ZSSRichTextEditorToolbarH2,
+                                 ZSSRichTextEditorToolbarH3,
+                                 ZSSRichTextEditorToolbarTextColor,
+                                 ZSSRichTextEditorToolbarBackgroundColor,
+                                 ZSSRichTextEditorToolbarUnorderedList,
+                                 ZSSRichTextEditorToolbarOrderedList,
+                                 ZSSRichTextEditorToolbarHorizontalRule];
     
-    // Set the base URL if you would like to use relative links, such as to images.
-    self.baseURL = [NSURL URLWithString:@"http://www.zedsaid.com"];
-    self.shouldShowKeyboard = NO;
-    // Set the HTML contents of the editor
-    [self setPlaceholder:@"This is a placeholder that will show when there is no content(html)"];
-    
+    NSString *html = @"<br><br>"
+    "<p>来自凌云App</p>";
     [self setHTML:html];
 }
 
 - (IBAction)showContact:(id)sender {
+    if(!sender){
+        return;
+    }
+        
     [self.view layoutIfNeeded]; //改变量影响到的最终视图做layoutIfNeeded
     [UIView animateWithDuration:0.3 delay: 0 options: UIViewAnimationOptionCurveEaseInOut animations:^{
         tableViewTopContraint.constant = tableViewTopContraint.constant == 0? -667: 0;
@@ -154,42 +158,6 @@
     return cell;
 }
 
-
-#pragma mark - ZSSRichTextEditor
-- (void)showInsertURLAlternatePicker {
-    
-    [self dismissAlertView];
-    
-    
-}
-
-
-- (void)showInsertImageAlternatePicker {
-    
-    [self dismissAlertView];
-    
-    
-}
-
-- (void)editorDidChangeWithText:(NSString *)text andHTML:(NSString *)html {
-    
-    NSLog(@"Text Has Changed: %@", text);
-    
-    NSLog(@"HTML Has Changed: %@", html);
-    
-}
-
-- (void)hashtagRecognizedWithWord:(NSString *)word {
-    
-    NSLog(@"Hashtag has been recognized: %@", word);
-    
-}
-
-- (void)mentionRecognizedWithWord:(NSString *)word {
-    
-    NSLog(@"Mention has been recognized: %@", word);
-    
-}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
